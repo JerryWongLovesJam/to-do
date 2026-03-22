40a817da8d3e6ca1b72021dc0383b5597b5cd153
JerryWongLovesJam <james.wu23@education.nsw.gov.au>
2026-03-22 23:03:22 +0000

    Finished the table and started writing the html

--- todo_page.py
+++ todo_page.py
@@ -1,17 +1,49 @@
+"""Creates a webserver for interacting with the todo database."""
+# Imports required modules
 import sqlite3
 from bottle import route, run
 
-def execute_query():
+def execute_query(query, params=(), fetch=False):
+    """Handles database queries and interactions."""
+    # Connects to the database
+    with sqlite3.connect('todo.db') as conn:
+        # Executes SQL queru and returns result
+        cur = conn.execute(query, params)
+        return cur.fetchall() if fetch else None
 
 @route('/')
 def todo_list():
-    conn = sqlite3.connect('todo.db')
-    cur = conn.cursor()
+    """Set path to the home page"""
+    rows = execute_query("SELECT id, category, item FROM todo ORDER BY category, item", fetch=True)
 
-    cur.execute("SELECT * FROM todo")
-    result = cur.fetchall()
-    cur.close()
+    table_rows = ""
+    for row in rows:
+        row_id, category, item = row
 
-    return str(result)
+        table_rows += f"""
+        # html language
+        <tr>
+            <td>{category}</td>
+            <td>{item}</td>
+            <td>    
+                <form action='/delete' method='POST'>
+                    <input type='hidden' name=delitem value='{row_id}'>
+                    <button type='submit'>Delete</button>
+                </form>
+            </td>
+        </tr>
+        """
 
+        html = f"""
+        <!DOCTYPE html>
+        <html>
+        <head>
+            <title>To-do List</title>
+        </head>
+        <body>
+        </body>
+        </html>
+        """
+
+# Starts the webserver
 run(host = 'localhost', port = 8080)
